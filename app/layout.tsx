ea195f7942a3a016742f0bd09bd3a533a5d9df76
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-24 17:06:18 +0900

    Renew sign in page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Figtree } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={figtree.className}>{children}</body>
     </html>
   );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,42 +2,52 @@ import { login, signup } from "./actions";
 
 export default function LoginPage() {
   return (
-    <div className="text-center flex flex-col items-center justify-center w-screen h-screen">
-      <h1 className="text-xl font-bold mb-4">Sign In / Sign Up</h1>
-      <form className="flex flex-col">
-        <div className="flex flex-col mb-4">
-          <label htmlFor="email">Email</label>
-          <input
-            className="px-4 py-2 border border-gray-200 rounded-lg mb-2"
-            id="email"
-            name="email"
-            type="email"
-            required
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            className="px-4 py-2 border border-gray-200 rounded-lg mb-2"
-            id="password"
-            name="password"
-            type="password"
-            required
-          />
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <div className="w-60 h-80 flex-col gap-12 flex">
+        <div className="text-gray-900 text-2xl font-medium text-center">
+          Welcome
         </div>
-        <div className="flex gap-2">
-          <button
-            className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-lg"
-            formAction={login}
-          >
-            Log in
-          </button>
-          <button
-            className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-lg"
-            formAction={signup}
-          >
-            Sign up
-          </button>
-        </div>
-      </form>
+        <form className="flex-col gap-12 flex">
+          <div className="flex-col gap-6 flex">
+            <div className="flex-col gap-2 flex">
+              <label htmlFor="email" className="text-gray-500 text-sm">
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                className="px-4 h-10 bg-gray-50 rounded-2xl border border-gray-100"
+              />
+            </div>
+            <div className="flex-col gap-2 flex">
+              <label htmlFor="password" className="text-gray-500 text-sm">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                className="px-4 h-10 bg-gray-50 rounded-2xl border border-gray-100"
+              />
+            </div>
+          </div>
+          <div className="self-stretch gap-4 flex">
+            <button
+              formAction={login}
+              className="h-10 px-4 py-2 bg-gray-800 rounded-2xl text-white flex-1"
+            >
+              Sign In
+            </button>
+            <button
+              formAction={signup}
+              className="h-10 px-4 py-2 bg-gray-800 rounded-2xl text-white flex-1"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
