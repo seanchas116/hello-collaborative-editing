bdb319ebb777b1055bdbc4bc1bc0143368248e0d
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-24 17:26:31 +0900

    Renew top page

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,40 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="w-screen h-screen flex items-center justify-center">
-      <div className="flex flex-col gap-8 text-center items-center">
-        <div>
-          <h1 className="text-4xl font-bold mb-4">
-            Hello Collaborative Editing
-          </h1>
-          <p>
-            Collaborative editor app example using Next.js, Supabase, Cloudflare
-            Durable Objects and Yjs.
-          </p>
+      <div className="flex-col items-center gap-16 flex">
+        <div className="flex-col items-center gap-12 flex">
+          <div className="text-center text-gray-900 text-5xl font-medium leading-[4rem]">
+            Collaborative
+            <br />
+            Note Taking App Example
+          </div>
+          <div className="items-center gap-3 flex text-base font-medium text-gray-600">
+            <div>Next.js</div>
+            <div className="w-2 h-2 bg-gray-200 rounded" />
+            <div>Supabase</div>
+            <div className="w-2 h-2 bg-gray-200 rounded" />
+            <div>Cloudflare Durable Objects</div>
+            <div className="w-2 h-2 bg-gray-200 rounded" />
+            <div>Y.js</div>
+            <div className="w-2 h-2 bg-gray-200 rounded" />
+            <div>Tiptap</div>
+          </div>
         </div>
-        <Link
-          className="bg-gray-800 text-white py-2 px-4 rounded"
-          href="/login"
-        >
-          Start Editing
-        </Link>
+        <div className="gap-4 flex">
+          <Link
+            href="/login"
+            className="px-6 py-3 bg-gray-800 hover:bg-gray-700 rounded-3xl gap-2.5 flex text-white text-base font-medium"
+          >
+            Launch App
+          </Link>
+          <a
+            href="https://github.com/seanchas116/hello-collaborative-editing"
+            target="_blank"
+            className="px-6 py-3 rounded-3xl gap-2.5 flex text-gray-900 text-base font-medium"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
     </main>
   );
