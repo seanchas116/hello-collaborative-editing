a08d57177afff8fd163a6fb2e98914bb2ea19476
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-26 12:31:31 +0900

    Add user dropdown

--- packages/app/app/editor/SideBar.tsx
+++ packages/app/app/editor/SideBar.tsx
@@ -3,6 +3,14 @@ import { Icon } from "@/components/Icon";
 import { ReactTimeAgo } from "@/components/TimeAgo";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const SideBar: React.FC<{
   files: File[];
@@ -23,13 +31,23 @@ export const SideBar: React.FC<{
   return (
     <nav className="w-[256px] bg-gray-50 h-screen flex flex-col text-sm border-r border-gray-200">
       <div className="m-2">
-        <div className="items-center flex gap-2 p-2">
-          <img
-            className="w-8 h-8 rounded-2xl"
-            src="https://via.placeholder.com/32x32"
-          />
-          <div className="text-gray-900 font-medium text-sm">Jane Doe</div>
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="items-center flex gap-2 p-2">
+            <img
+              className="w-8 h-8 rounded-2xl"
+              src="https://via.placeholder.com/32x32"
+            />
+            <div className="text-gray-900 font-medium text-sm">Jane Doe</div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <div className="p-2 flex items-center gap-2 text-gray-400 py-0">
           <div className="p-2 rounded-full">
             <Icon icon="material-symbols:search" className="text-base" />
