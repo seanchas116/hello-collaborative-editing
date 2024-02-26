e154addf512f398d6c6ab819fcc529a90771b3d2
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-26 13:18:23 +0900

    WIP: authenticate in worker

--- packages/worker/src/Bindings.ts
+++ packages/worker/src/Bindings.ts
@@ -13,4 +13,6 @@ export type Bindings = {
 	//
 	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
 	// MY_QUEUE: Queue;
+
+	CF_WORKER_JWT_SECRET: string;
 };
--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import jwt from 'jsonwebtoken';
 import { Bindings } from './Bindings';
 export { FileDurableObject } from './FileDurableObject';
 
@@ -6,7 +7,7 @@ const app = new Hono<{
 	Bindings: Bindings;
 }>();
 
-// GET /file?id=123
+// GET /file?id=123&token=abc
 app.get('/file', async (c) => {
 	console.log('get /file');
 
@@ -15,8 +16,34 @@ app.get('/file', async (c) => {
 		return c.text('id is required', 404);
 	}
 
+	const tokens = c.req.queries('token');
+	if (tokens?.length !== 1) {
+		return c.text('token is required', 404);
+	}
+
+	const token = tokens[0];
+	const secret = c.env.CF_WORKER_JWT_SECRET;
+
+	try {
+		const decoded = jwt.verify(token, secret);
+		if (typeof decoded !== 'object') {
+			return c.text('invalid token', 403);
+		}
+		if (!decoded.exp || decoded.exp < Math.floor(Date.now() / 1000)) {
+			return c.text('token expired', 403);
+		}
+		if (typeof decoded.sub !== 'string') {
+			return c.text('invalid token', 403);
+		}
+
+		// TODO: check if the user has access to the file
+	} catch (e) {
+		return c.text('invalid token', 403);
+	}
+
 	const id = c.env.FILE_DURABLE_OBJECT.idFromName(fileID[0]);
 	const obj = c.env.FILE_DURABLE_OBJECT.get(id);
+
 	return await obj.fetch(c.req.raw);
 });
 
