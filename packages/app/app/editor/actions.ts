40a824d6f92ac77956c9924214672609ed607421
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-26 13:06:47 +0900

    Add generateCollaborativeAuthToken

--- packages/app/app/editor/actions.ts
+++ packages/app/app/editor/actions.ts
@@ -5,6 +5,7 @@ import { files } from "@/db/schema";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import jwt from "jsonwebtoken";
 
 export async function createFile() {
   const supabase = createClient();
@@ -26,7 +27,7 @@ export async function createFile() {
   redirect(`/editor?file=${file.id}`);
 }
 
-export async function getFiles() {
+export async function generateCollaborativeAuthToken() {
   const supabase = createClient();
 
   const { data, error } = await supabase.auth.getUser();
@@ -34,5 +35,19 @@ export async function getFiles() {
     throw new Error("User not found");
   }
 
-  return await db.select().from(files);
+  const secret = process.env.CF_WORKER_JWT_SECRET;
+  if (!secret) {
+    throw new Error("No secret found");
+  }
+
+  const token = jwt.sign(
+    {
+      sub: data.user.id,
+      // 1 hour
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+    },
+    secret
+  );
+
+  return token;
 }
