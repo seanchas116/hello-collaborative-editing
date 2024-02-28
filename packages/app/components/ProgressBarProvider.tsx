dc2e52d6425a0022407a27e695b2c8c9e43c6d15
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-28 22:28:37 +0900

    Add Suspense

--- packages/app/components/ProgressBarProvider.tsx
+++ packages/app/components/ProgressBarProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
+import { Suspense } from "react";
 import colors from "tailwindcss/colors";
 
 export const ProgressBarProvider: React.FC<{
@@ -9,12 +10,14 @@ export const ProgressBarProvider: React.FC<{
   return (
     <>
       {children}
-      <ProgressBar
-        // height="4px"
-        // color={colors.blue[500]}
-        // options={{ showSpinner: false }}
-        shallowRouting
-      />
+      <Suspense fallback={<></>}>
+        <ProgressBar
+          // height="4px"
+          // color={colors.blue[500]}
+          // options={{ showSpinner: false }}
+          shallowRouting
+        />
+      </Suspense>
     </>
   );
 };
