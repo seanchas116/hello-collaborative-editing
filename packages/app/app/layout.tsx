f080e8ba2dc5e8e57c9cf70eff53fd575c0652db
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-28 22:35:34 +0900

    Remove next-nprogress-bar

--- packages/app/app/layout.tsx
+++ packages/app/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-import { ProgressBarProvider } from "@/components/ProgressBarProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ProgressBarProvider>{children}</ProgressBarProvider>
+        {children}
         <Toaster />
       </body>
     </html>
