7acec98a8ec3c754edfac7a04d5f3794ab28baaf
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-29 15:38:24 +0900

    Tweak style

--- packages/app/app/editor/[id]/ShareButton.tsx
+++ packages/app/app/editor/[id]/ShareButton.tsx
@@ -4,6 +4,7 @@ import { PopoverTrigger } from "@radix-ui/react-popover";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
 
 export const ShareButton: React.FC<{
   className?: string;
@@ -11,51 +12,59 @@ export const ShareButton: React.FC<{
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button className={className}>Share</Button>
+        <button
+          className={cn(
+            "bg-blue-500 rounded-full px-3 py-1.5 text-sm text-white",
+            className
+          )}
+        >
+          Share
+        </button>
       </PopoverTrigger>
-      <PopoverContent className="w-80 text-sm flex flex-col gap-4" align="end">
-        <div>
-          <h2 className="font-semibold mb-2">Share this file</h2>
-          <div className="flex gap-2">
+      <PopoverContent
+        className="w-[400px] text-sm flex flex-col p-0 rounded-xl border-gray-100 text-gray-900"
+        align="end"
+      >
+        <div className="p-4">
+          <h2 className="font-medium mb-4">Share this file</h2>
+          <div className="flex gap-4 items-center">
             <Input placeholder="Email Address" />
-            <Button variant="secondary" className="shrink-0">
+            <button className="bg-blue-500 rounded-full px-3 py-1.5 text-sm text-white">
               Invite
-            </Button>
+            </button>
           </div>
         </div>
-        <div className="h-px bg-gray-100 -mx-4" />
-        <div className="space-y-4">
-          <div className="grid gap-6">
-            <div className="flex items-center justify-between gap-4">
-              <div className="flex items-center gap-4">
-                <Avatar>
-                  <AvatarImage src="/avatars/03.png" alt="Image" />
-                  <AvatarFallback>OM</AvatarFallback>
-                </Avatar>
-                <div>
-                  <p className="text-sm font-medium leading-none">
-                    Olivia Martin
-                  </p>
-                  <p className="text-sm text-muted-foreground">m@example.com</p>
-                </div>
+        <div className="h-px bg-gray-100" />
+        <div className="flex flex-col p-4 gap-4">
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-4">
+              <Avatar>
+                <AvatarImage src="/avatars/03.png" alt="Image" />
+                <AvatarFallback>OM</AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="text-sm font-medium leading-none">
+                  Olivia Martin
+                </p>
+                <p className="text-sm text-muted-foreground">m@example.com</p>
               </div>
-              <div>Owner</div>
             </div>
-            <div className="flex items-center justify-between space-x-4">
-              <div className="flex items-center space-x-4">
-                <Avatar>
-                  <AvatarImage src="/avatars/05.png" alt="Image" />
-                  <AvatarFallback>IN</AvatarFallback>
-                </Avatar>
-                <div>
-                  <p className="text-sm font-medium leading-none">
-                    Isabella Nguyen
-                  </p>
-                  <p className="text-sm text-muted-foreground">b@example.com</p>
-                </div>
+            <div>Owner</div>
+          </div>
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-4">
+              <Avatar>
+                <AvatarImage src="/avatars/05.png" alt="Image" />
+                <AvatarFallback>IN</AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="text-sm font-medium leading-none">
+                  Isabella Nguyen
+                </p>
+                <p className="text-sm text-muted-foreground">b@example.com</p>
               </div>
-              <div>Can Edit</div>
             </div>
+            <div>Can Edit</div>
           </div>
         </div>
       </PopoverContent>
